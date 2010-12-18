e5e118e260fbc8076d44c2c8bee3eab76816bf21
dennis <dennis.iversen@gmail.com>
2010-12-18 13:10:25 +0100

    added option for edit main menu items.

---
+++ menu/titles.php
@@ -0,0 +1,9 @@
+<?php
+
+settings_view_titles();
+if (isset($_POST['submit'])){
+    $menu = new menus();
+    $menu->updateMenuTitles ();
+    session::setActionMessage(lang::translate('settings_menu_titles_updated'));
+    header("Location: /settings/menu/titles");
+}

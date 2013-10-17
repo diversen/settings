6282f7b712d0143eeaf19b13972707f83e010079
dennis <dennis@dennis-Inspiron-531.(none)>
2013-10-17 18:40:03 +0200

    minor edit

--- template/index.php
+++ template/index.php
@@ -11,13 +11,16 @@ template::setTitle(lang::translate('Edit Template'));
 $t = new settings_template();
 $values = $t->getTemplate('update');
 
-if (isset($_POST)) $_POST = html::specialEncode ($_POST);
+if (isset($_POST)) { 
+    $_POST = html::specialEncode ($_POST);
+}
+
 if (isset($_POST['template'])){
     $t->validateTemplate();
     if (empty($t->errors)){
         $t->updateTemplate();
         session::setActionMessage(lang::translate('settings_template_updated'));
-        http::locationHeader('/settings/template/index' );
+        http::locationHeader('/settings/template/index');
     } 
 } else {
     settings_template($values);

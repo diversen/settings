4a95d5b9ca4b75bf843746392012064d4b4cbab2
Dennis Iversen <dennis.iversen@gmail.com>
2014-07-03 16:26:41 +0200

    clean up

--- template/index.php
+++ template/index.php
@@ -2,7 +2,7 @@
 
 http::prg();
 
-if (!session::checkAccessFromModuleIni('settings_allow_edit')){
+if (!session::checkAccessFromModuleIni('settings_allow_edit')) {
     return;
 }
 
@@ -11,24 +11,22 @@ template::setTitle(lang::translate('Edit Template'));
 $t = new settings_template();
 $values = $t->getTemplate('update');
 
-if (isset($_POST)) { 
-    $_POST = html::specialEncode ($_POST);
+if (isset($_POST)) {
+    $_POST = html::specialEncode($_POST);
 }
 
-if (isset($_POST['template'])){
+if (isset($_POST['template'])) {
     $t->validateTemplate();
-    if (empty($t->errors)){
+    if (empty($t->errors)) {
         $t->updateTemplate();
         session::setActionMessage(lang::translate('settings_template_updated'));
         http::locationHeader('/settings/template/index');
-    } 
+    }
 }
 
-if (!empty($t->errors)){
-
-        echo html::getErrors($t->errors);
-
-    }
-            settings_template($values);
-        $t->getTemplates(); 
-        return;
+if (!empty($t->errors)) {
+    echo html::getErrors($t->errors);
+}
+settings_template($values);
+$t->getTemplates();
+return;

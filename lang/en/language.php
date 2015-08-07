ddf5330f3e4077ed3dfb2266ba59483c0d570b71
Dennis Iversen <dennis.iversen@gmail.com>
2015-08-07 18:36:08 +0200

    refactor lang

---
+++ lang/en/language.php
@@ -0,0 +1,83 @@
+<?php
+
+$LANG = array();
+
+// Translation of file modules/settings/template/module.php
+
+$LANG['Edit Template'] = 'Edit Template';
+$LANG['settings_template_updated'] = 'settings_template_updated';
+$LANG['Edit Template Css'] = 'Edit Template Css';
+$LANG['settings_css_updated'] = 'settings_css_updated';
+$LANG['Select a template'] = 'Select a template';
+// Translation of file modules/settings/template/views.php
+
+$LANG['Edit Template'] = 'Edit Template';
+$LANG['Select template'] = 'Select template';
+$LANG['Update'] = 'Update';
+$LANG['Edit Stylesheet'] = 'Edit Stylesheet';
+$LANG['Select Stylesheet'] = 'Select Stylesheet';
+// Translation of file modules/settings/module.php
+
+$LANG['Site Settings'] = 'Site Settings';
+// Translation of file modules/settings/mail/module.php
+
+$LANG['settings: action message: mail method updated'] = 'settings: action message: mail method updated';
+$LANG['settings: action message: smtp updated'] = 'settings: action message: smtp updated';
+$LANG['settings: action message: sendmail updated'] = 'settings: action message: sendmail updated';
+$LANG['settings: action message: site email updated'] = 'settings: action message: site email updated';
+$LANG['settings: error: not a valid mail method'] = 'settings: error: not a valid mail method';
+$LANG['settings: error: all smtp params needs to be set'] = 'settings: error: all smtp params needs to be set';
+$LANG['settings: mail method legend'] = 'settings: mail method legend';
+$LANG['settings: mail label type'] = 'settings: mail label type';
+$LANG['Submit'] = 'Submit';
+$LANG['settings: mail smtp legend'] = 'settings: mail smtp legend';
+$LANG['settings: label mail smtp host'] = 'settings: label mail smtp host';
+$LANG['settings: label mail smtp port'] = 'settings: label mail smtp port';
+$LANG['settings: label mail smtp username'] = 'settings: label mail smtp username';
+$LANG['settings: label mail smtp password'] = 'settings: label mail smtp password';
+$LANG['settings: label mail smtp auth'] = 'settings: label mail smtp auth';
+$LANG['settings: label mail sendmail path'] = 'settings: label mail sendmail path';
+$LANG['settings: label mail sendmail args'] = 'settings: label mail sendmail args';
+$LANG['submit'] = 'submit';
+$LANG['settings: mail php legend'] = 'settings: mail php legend';
+$LANG['settings: label mail php params'] = 'settings: label mail php params';
+$LANG['settings: mail email system legend'] = 'settings: mail email system legend';
+$LANG['settings: label mail system site email'] = 'settings: label mail system site email';
+// Translation of file modules/settings/mail/menu.inc
+
+$LANG['settings: menu mail system mail'] = 'settings: menu mail system mail';
+$LANG['settings: menu mail method'] = 'settings: menu mail method';
+$LANG['settings: menu mail params'] = 'settings: menu mail params';
+// Translation of file modules/settings/logo/module.php
+
+$LANG['Edit Logo'] = 'Edit Logo';
+// Translation of file modules/settings/install.inc
+
+$LANG['Look and feel'] = 'Look and feel';
+$LANG['SMTP Mail'] = 'SMTP Mail';
+// Translation of file modules/settings/system_lang.inc
+
+$LANG['settings'] = 'settings';
+// Translation of file modules/settings/menu.inc
+
+$LANG['Change Logo'] = 'Change Logo';
+$LANG['Change favicon'] = 'Change favicon';
+$LANG['Set template'] = 'Set template';
+$LANG['Edit Template Css'] = 'Edit Template Css';
+// Translation of file modules/settings/background/module.php
+
+$LANG['settings_favicon_title'] = 'settings_favicon_title';
+// Translation of file modules/settings/favicon/module.php
+
+$LANG['settings_favicon_title'] = 'settings_favicon_title';
+// Translation of file modules/settings/lib/image_form.php
+
+$LANG['settings_upload_no_file_specified'] = 'settings_upload_no_file_specified';
+$LANG['settings_upload_wrong_filetype_specified'] = 'settings_upload_wrong_filetype_specified';
+$LANG['Edit Logo'] = 'Edit Logo';
+$LANG['Image updated'] = 'Image updated';
+$LANG['Image deleted'] = 'Image deleted';
+$LANG['Update image'] = 'Update image';
+$LANG['Update'] = 'Update';
+$LANG['Delete image'] = 'Delete image';
+$LANG['Delete'] = 'Delete';
